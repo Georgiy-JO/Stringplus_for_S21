c8012d5f397a4faf1d56b17aaa661099812f2c2e
Georgiy-JO <wkone.mail@gmail.com>
2024-01-08 06:47:16 +0700

    Minor changes

--- rk_jo_br.sh
+++ rk_jo_br.sh
@@ -1,2 +1,6 @@
 git push origin rk_jo_br
+echo ◑ ◔
+echo ╔═╗
+echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+echo ╚═╝
 git push origin2 rk_jo_br 
---
+++ src/tester.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "s21_string.h"
+
+int main(){
+    
+}
