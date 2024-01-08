690d27d45d61eceab20c9eca5c7ebb9094c85715
Georgiy-JO <wkone.mail@gmail.com>
2024-01-08 09:34:09 +0700

    minor changies

--- src/s21_len.c
+++ src/s21_len.c
@@ -2,6 +2,15 @@
 
 
 size_t s21_strlen(const char *str){
-
-    
+    size_t length=0;
+    for(;str[length]!=NULL;length++);
+    return length;
 }
+
+/*posible thin places:
+    1)too long
+    2)NULL line
+    3)\n
+    4)\t
+    5)other zero-chars
+*/
--- src/s21_string.h
+++ src/s21_string.h
@@ -6,8 +6,8 @@
 #define size_t unsigned long int
 #define NULL ((void *)0)
 
-void *s21_memchr(const void *str, int c, size_t n);
-int s21_memcmp(const void *str1, const void *str2, size_t n);
+//void *s21_memchr(const void *str, int c, size_t n);
+//int s21_memcmp(const void *str1, const void *str2, size_t n);
 
 void *s21_memcpy(void *dest, const void *src, size_t n);
 size_t s21_strlen(const char *str);
