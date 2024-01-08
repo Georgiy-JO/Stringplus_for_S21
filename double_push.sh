13bedd2f709ce7ad1a551f69802603c89e02afd8
Georgiy-JO <wkone.mail@gmail.com>
2024-01-08 18:31:18 +0700

    deleted:    double_push.sh 	deleted:    src/Makefile_with_folders

--- double_push.sh
+++
@@ -1,8 +0,0 @@
-git commit -m'minor changies'
-git push origin feature/rk_jo_1
-echo ◑ ◔
-echo ╔═╗
-echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-echo ╚═╝
-git push origin2 feature/rk_jo_1
-
