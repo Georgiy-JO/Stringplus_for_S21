26749c4b530cada1d344754db05095d075d8c360
Georgiy-JO <wkone.mail@gmail.com>
2024-01-08 08:03:52 +0700

    minor changies

--- rk_jo_br.sh
+++ rk_jo_br.sh
@@ -1,6 +1,7 @@
-git push origin rk_jo_br
+git commit -m'minor changies'
+git push origin rk_jo_1
 echo ◑ ◔
 echo ╔═╗
 echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 echo ╚═╝
-git push origin2 rk_jo_br 
+git push origin2 rk_jo_1
