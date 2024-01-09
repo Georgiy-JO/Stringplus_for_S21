3e433061a001f0bb11b7cc9cf440a3144085de03
Georgiy-JO <wkone.mail@gmail.com>
2024-01-09 15:17:02 +0700

    modified:   double_push.sh

--- double_push.sh
+++ double_push.sh
@@ -1,8 +1,19 @@
 #git commit -m'minor changies'
-git push origin feature/rk_jo_1
-echo ◑ ◔
-echo ╔═╗
-echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-echo ╚═╝
-git push origin2 feature/rk_jo_1
+if [$1 -eq 1]
+then
+  git push origin feature/rk_jo_1
+  echo ◑ ◔
+  echo ╔═╗
+  echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+  echo ╚═╝
+  git push origin2 feature/rk_jo_1
+else
+  git push origin develop
+  echo ◑ ◔
+  echo ╔═╗
+  echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+  echo ╚═╝
+  git push origin2 develop
+fi
+
 
