81cce4edb6616bc2d9698fe150e0c3c3771ab82c
t1m0f3y <t.leonenko2001@gmail.com>
2024-01-12 09:38:18 +0700

    add s21_strncmp and s21_strchr

--- src/s21_string.h
+++ src/s21_string.h
@@ -16,8 +16,8 @@ void *s21_memset(void *str, int c, size_t n);
 // char *s21_strncpy(char *dest, const char *src, size_t n);
 
 // char *s21_strncat(char *dest, const char *src, size_t n);
-// char *s21_strchr(const char *str, int c);
-// int s21_strncmp(const char *str1, const char *str2, size_t n);
+char *s21_strchr(const char *str, int c);
+int s21_strncmp(const char *str1, const char *str2, size_t n);
 // size_t s21_strcspn(const char *str1, const char *str2);
 // char *s21_strerror(int errnum);
 // char *s21_strpbrk(const char *str1, const char *str2);
