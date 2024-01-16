557361a394b15356fe49d7f6d43e49667be25623
t1m0f3y <t.leonenko2001@gmail.com>
2024-01-16 10:38:37 +0700

    add s21_strerror for linux (ubuntu), BUT
    it works for ubuntu, but
    not for alpine (who cares?)
    There are a different lists of
    errors for each linux distro in string.h
    (in this case ubuntu and alpine)

--- src/s21_string.h
+++ src/s21_string.h
@@ -25,7 +25,7 @@ char *s21_strtok(char *str, const char *delim);                                 
 char *s21_strchr(const char *str, int c);
 int s21_strncmp(const char *str1, const char *str2, size_t n);
 // size_t s21_strcspn(const char *str1, const char *str2);
-// char *s21_strerror(int errnum);
+char *s21_strerror(int errnum);
 // char *s21_strpbrk(const char *str1, const char *str2);
 char *s21_strrchr(const char *str, int c);
 char *s21_strstr(const char *haystack, const char *needle);
