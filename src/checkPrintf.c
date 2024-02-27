950e31bca394fb846c2240acfeea44f4b3dfd9ec
t1m0f3y <t.leonenko2001@gmail.com>
2024-02-27 16:38:54 +0700

    TODO: fix a float
    see a checkPrintf.c file for tests

--- src/checkPrintf.c
+++ src/checkPrintf.c
@@ -8,8 +8,9 @@ int main(){
 	int digit = 344; //d i g G o u x X
 	int digit2 = -344; //d i g G o u x X
 	int udigit = -34; //d i g G o u x X
-	float fdigit = -21.2100210012120021312; //e E f
-	float fdigit2 = 21.111111; //e E f
+	float fdigit = 21.2100210012120021312; //e E f
+	float fdigit2 = -21.2100210012120021312; //e E f
+	//float fdigit2 = 21.111111; //e E f
 	float fdigit3 = -0.000001; //e E f
 	char* str = "hello"; //s
 	int *ptr = &digit; //p
@@ -19,8 +20,8 @@ int main(){
 	int count = 0; 
 	//s21_count = s21_sprintf(buffer, "---\n%%%% hel%%lo %-40.10d %8.4d\nchar is %c\nfloat is %f |%+-20.10f| %f\nstr is %s%s\n%40.20u\n%d", digit, (int)fdigit, c, fdigit, fdigit2, fdigit3, str, str, udigit);
 	//count = sprintf(buffer2, "---\n%%%% hel%%lo %-40.10d %8.4d\nchar is %c\nfloat is %f |%+-20.10f| %f\nstr is %s%s\n%40.20u\n%d", digit, (int)fdigit, c, fdigit, fdigit2, fdigit3, str, str, udigit);
-	s21_count = s21_sprintf(buffer, "%d %d\n%10d %10d\n%-10d %-10d\n%.10d %.10d\n%-.10d %-.10d\n%8.10d %8.10d\n%-8.10d %-8.10d\n%10.8d %10.8d\n%-10.8d %-10.8d\n", digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2);
-	count = sprintf(buffer2, "%d %d\n%10d %10d\n%-10d %-10d\n%.10d %.10d\n%-.10d %-.10d\n%8.10d %8.10d\n%-8.10d %-8.10d\n%10.8d %10.8d\n%-10.8d %-10.8d\n", digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2, digit, digit2);
+	s21_count = s21_sprintf(buffer, "%f %f\n|%20f| |%20f|\n%-20f %-20f\n%.20f %.20f\n%-.20f %-.20f\n%8.20f %8.20f\n%-8.20f %-8.20f\n|%20.8f| |%20.8f|\n%-20.8f %-20.8f\n", fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2);
+	count = sprintf(buffer2, "%f %f\n|%20f| |%20f|\n%-20f %-20f\n%.20f %.20f\n%-.20f %-.20f\n%8.20f %8.20f\n%-8.20f %-8.20f\n|%20.8f| |%20.8f|\n%-20.8f %-20.8f\n", fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2, fdigit, fdigit2);
 	printf("%s", buffer);
 	printf("\n----\n");
 	printf("%s\n", buffer2);
