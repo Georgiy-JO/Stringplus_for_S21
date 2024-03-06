3f9d0ef8dd32f5af093dffc33a6a057dc6e6addb
Georgiy-JO <wkone.mail@gmail.com>
2024-03-06 23:32:03 +0700

    Step_3.1. ~FINAL FORM~         Commit with some author's comments.             Note: This commit is for myself.
        Status:     s21_sscanf works;
                    Tests for "s21_sscanf" are done;
                    Oprimization finished;
                    Uninformative comments cleaned;
                    Debugging comments cleaned.

--- double_push.sh
+++ double_push.sh
@@ -4,12 +4,12 @@ old_tec_com=$(<"$VAR_FILE")
 
 if [ "$1" -eq 1 ]
 then
-  git push origin feature/rk_jo_rework_check
+  git push origin feature/rk_jo_final
   echo ◑ ◔
   echo ╔═╗
   echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
   echo ╚═╝
-  git push origin2 feature/rk_jo_rework_check
+  git push origin2 feature/rk_jo_final
 elif [ $1 -eq 2 ]
 then
   git push origin develop
@@ -26,6 +26,14 @@ then
   echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
   echo ╚═╝
   git push origin2 feature/rk_jo_2
+elif [ $1 -eq 4 ]
+then
+  git push origin feature/rk_jo_rework_check
+  echo ◑ ◔
+  echo ╔═╗
+  echo ║▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+  echo ╚═╝
+  git push origin2 feature/rk_jo_rework_check
 elif [ $1 -eq 9 ]
 then
   new_tec_com=$((old_tec_com + 1))
