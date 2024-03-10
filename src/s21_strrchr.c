0dff94cd5a989746f0c7e607c3ec55c213248b67
t1m0f3y <t.leonenko2001@gmail.com>
2024-03-10 22:58:44 +0700

    fix s21_strchr s21_strncmp s21_strrchr s21_strstr

--- src/s21_strrchr.c
+++ src/s21_strrchr.c
@@ -1,13 +1,14 @@
 #include "s21_string.h"
 
 char* s21_strrchr(const char* _str, int c) {
+  size_t len = s21_strlen(_str);
   unsigned char* ptr = NULL;
   unsigned char* str = (unsigned char*)_str;
-  while (*str != '\0') {
-    if (*str == c) {
-      ptr = str;
-    }
-    str++;
+  for (int i = (int)len + 1; i >= 0; i--){
+	  if (str[i] == c) {
+		  ptr = &(str[i]);
+		  break;
+	  }
   }
   return (char*)ptr;
 }
--- src/s21_strstr.c
+++ src/s21_strstr.c
@@ -1,30 +1,24 @@
 #include "s21_string.h"
 
 char* s21_strstr(const char* haystack, const char* needle) {
-  unsigned char* ptr = NULL;
-  unsigned char* haystackorig = (unsigned char*)haystack;
-  unsigned char* needleorig = (unsigned char*)needle;
-  unsigned char* haystacktmp = (unsigned char*)haystack;
-  unsigned char* needletmp = (unsigned char*)needle;
-  int status = 0;
-  if (*needleorig == '\0') {
-    ptr = haystackorig;
-  } else {
-    while (*haystackorig != '\0') {
-      haystacktmp = haystackorig;
-      needletmp = needleorig;
-      status = 1;
-      while (*haystacktmp != '\0' && status) {
-        if (*haystacktmp != *needletmp) {
-          status = 0;
-        } else {
-          haystacktmp++;
-          needletmp++;
-        }
-      }
-      if (status == 1) ptr = haystackorig;
-      haystackorig++;
-    }
-  }
-  return (char*)ptr;
+	const char* result = NULL;
+	if (*needle == '\0') {
+		result = haystack;
+	} else {
+		while (*haystack != '\0') {
+			const char* h = haystack;
+			const char* n = needle;
+
+			while (*n != '\0' && *h == *n) {
+				++h;
+				++n;
+			}
+			if (*n == '\0') {
+				result = haystack;
+				break;
+			}
+			++haystack;
+		}
+	}
+	return (char*)result;
 }
